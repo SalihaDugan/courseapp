bc61a42af2d2675f0b14e1c395bcef97d138e330
SalihaDugan <96032761+SalihaDugan@users.noreply.github.com>
2024-11-03 00:58:58 +0300

    Relationships-Pagination

--- courses/models.py
+++ courses/models.py
@@ -1,26 +1,25 @@
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(default="", null=False, unique=True, db_index=True, max_length=50)
+
+    
+    def __str__(self):
+        return f"{self.name}"
+
 class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     imageUrl = models.CharField(max_length=50, blank=False)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
     isActive = models.BooleanField()
-    slug = models.SlugField(default="", blank = True, editable = False, null=False, unique = True, db_index = True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(args,kwargs)
-
+    slug = models.SlugField(default="", blank = True, null=False, unique = True, db_index = True)
+    categories = models.ManyToManyField(Categories)
 
     def __str__(self):
         return f"{self.title}"
     
-class Categories(models.Model):
-    name = models.CharField(max_length=40)
-    slug = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return f"{self.name}"
 
--- courses/views.py
+++ courses/views.py
@@ -1,52 +1,7 @@
-from datetime import date, datetime
-from django.http import Http404,  HttpResponseNotFound
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.http import Http404
+from django.shortcuts import  render
 from .models import Categories, Course
-data = {
-    "programlama": "programlama kategorisine ait kurslar",
-    "web-gelistirme": "web geliştirme kategorisine ait kurslar",
-    "mobil": "mobil kategorisine ait kurslar",
-}
-
-db= {
-    "courses": [
-        {
-            "title":"javascript kursu",
-            "description": "javascript kurs açıklaması",
-            "imageUrl": "1.jpg",
-            "slug": "javascript-kursu",
-            "date": datetime.now(),
-            "isActive": True,
-            "isUpdated": False
-        },
-        {
-            "title":"python kursu",
-            "description": "python kurs açıklaması",
-            "imageUrl": "2.jpg",
-            "slug": "python-kursu",
-            "date": date(2022,10,10),
-            "isActive": True,
-            "isUpdated": False
-        },
-        {
-            "title":"web geliştirme kursu",
-            "description": "web geliştirme kurs açıklaması",
-            "imageUrl": "3.jpg",
-            "slug": "web-gelistirme-kursu",
-            "date": date(2022,10,10),
-            "isActive": True,
-            "isUpdated": True
-        },
-    ],
-    "categories": [
-        {"id": 1, "name": "programlama", "slug":"programlama"}, 
-        {"id": 2, "name": "web geliştirme", "slug":"web-gelistirme"},
-        {"id": 3, "name": "mobil uygulamalar", "slug":"mobil-uygulamalar"},
-        ]
-}
-# http://127.0.0.1:8000/kurslar
-
+from django.core.paginator import Paginator
 
 def index(request):
     # list comphension
@@ -71,25 +26,24 @@ def details(request, slug):
 
     return render(request, 'courses/details.html', context)
 
-def getCoursesByCategoryName(request, category_name):
-    try:
-        category_text = data[category_name]
-        return render(request, "courses/courses.html", {
-            'category': category_name,
-            'category_text': category_text
-        })
-    except:
-        return HttpResponseNotFound("yanlış kategori seçimi")
+def getCoursesByCategory(request, slug):
+    kurslar = Course.objects.filter(categories__slug = slug, isActive=True).order_by("date")
+    kategoriler = Categories.objects.all()
+
+    paginator = Paginator(kurslar, 3)
+    page = request.GET.get('page', 1)
+    page_obj = paginator.page(page)
+
+    print(paginator.count)
+    print(paginator.num_pages)
+
+    return render(request, 'courses/index.html' ,{
+
+        'categories': kategoriler,
+        'page_obj': page_obj,
+        'seciliKategori': slug,       
+    })
 
 
-def getCoursesByCategoryId(request, category_id):
-    category_list = list(data.keys())
-    if category_id > len(category_list):
-        return HttpResponseNotFound("yanlış kategori seçimi")
-    category = category_list[category_id - 1]
-
-    redirect_url = reverse('courses_by_category', args=[category])
-
-    return redirect(redirect_url)
 
 # Create your views here.
